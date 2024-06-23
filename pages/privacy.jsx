1b604fcefa3da4ca21e2304862d97df453bc133b
Freezipycraete <74733623+Freezipycraete@users.noreply.github.com>
2024-06-23 07:23:59 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -7,11 +7,11 @@ export default function Privacy() {
   <div className="animateHeader text-white text-opacity-40 mt-5">
         <p>⊳ To use the strixe bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
         <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot when using certain commands from the bot commands.</p>
+        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">strixe</a> bot when using certain commands from the bot commands.</p>
         <div className="py-3"></div>
         <p>⊳ Every user can use the MusicMaker bot however they want, as long as they do not intend to harm.</p>
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using MusicMaker.</p>   
+        <p>⊳ You cannot use certain commands at the same time while using strixe.</p>   
         <div className="py-3"></div>
         <p>⊳ strixe bot does not save any data based on users.</p>
         <div className="py-3"></div>
