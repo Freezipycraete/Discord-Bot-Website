c0517df3eb25b6ccca6f4ef0db0033c6820d40d9
Freezipycraete <74733623+Freezipycraete@users.noreply.github.com>
2024-06-23 04:35:27 +0300

    Update commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -89,7 +89,7 @@ const commands = [
 
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-  <Link href={"https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=applications.commands%20bot"}>
+  <Link href={"https://discord.com/oauth2/authorize?client_id=1238661891233026100&permissions=8&integration_type=0&scope=bot"}>
   <a className={"flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Invite MusicMaker</a>
   </Link>
